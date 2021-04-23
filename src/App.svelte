5fa689e208c51b8fcafe123634b9fe1758929b6f
Alejandro <alejpr@kth.se>
2021-04-23 22:35:29 +0200

    A ball that moves

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,90 @@
 <script lang="ts">
+import { prevent_default } from "svelte/internal";
+
     import Player from "./Player.svelte";
 
-    let nClicks : number = 0;
+    interface GameData {
+        scenarioSize : number[],
+        playerRadius : number
+    }
+
+    let gameData : GameData = {
+        scenarioSize : [1000, 600],
+        playerRadius : 25
+    }
+
+    let currentlyPressedKeys = {
+        ArrowUp: false,
+        ArrowDown: false,
+        ArrowLeft: false,
+        ArrowRight: false
+    }
+
+    let playerPosition = [0,0];
+    let playerSpeed = 100;
+    let currentTime;
+
+
+    function processKeyDown(e: KeyboardEvent) {
+        let keyPressed = e.key;
+        if (keyPressed !== "F12" && keyPressed !== "F5"){
+            e.preventDefault();
+        }
+        currentlyPressedKeys[keyPressed] = true;
+        console.log(currentlyPressedKeys);
+    }
+    function processKeyUp(e: KeyboardEvent) {
+        let keyPressed = e.key;
+        currentlyPressedKeys[keyPressed] = false;
+    }
+
+    document.onkeydown = processKeyDown;
+    document.onkeyup = processKeyUp;
+
+    function mainLoop(timeStamp: number) {
+
+        if (!currentTime){
+            currentTime = timeStamp;
+        }
+
+        let timestep = (timeStamp - currentTime)/1000;
+        currentTime = timeStamp;
+
+
+        if (currentlyPressedKeys["ArrowDown"]) {
+            playerPosition[1] += playerSpeed * timestep;
+        }
+        if (currentlyPressedKeys["ArrowUp"]) {
+            playerPosition[1] -= playerSpeed * timestep;
+        }
+        if (currentlyPressedKeys["ArrowRight"]) {
+            playerPosition[0] += playerSpeed * timestep;
+        }
+        if (currentlyPressedKeys["ArrowLeft"]) {
+            playerPosition[0] -= playerSpeed * timestep;
+        }       
+
+        requestAnimationFrame(mainLoop);
+    }
+
+    requestAnimationFrame(mainLoop);
 
 </script>
 
 <style>
     div {
-        background-color: green;
+        background-color:darkblue;
+        width: 1000px;
+        height: 600px;
+        margin: auto;
         -moz-user-select: none;
+        -webkit-user-select: none;
         cursor: pointer;
     }
 </style>
 
-<div on:click={()=> nClicks+=1}>
-    Click here {nClicks} times so far
+<div>
+    <Player position={playerPosition} />
 </div>
-<Player number={nClicks*2}/>
+
+
--- src/Player.svelte
+++ src/Player.svelte
@@ -1,14 +1,17 @@
-<script>
-    export let number;
+<script lang="ts">
+    export let position : number[];
 </script>
 
 <style>
     div {
-        background-color: purple;
-        color: seashell;
+        background-color: orange;
+        width: 50px;
+        height: 50px;
+        position: absolute;
+        border-radius: 25px;
     }
 </style>
 
-<div>
-    {number}
+<div style="transform: translate({position[0]}px, {position[1]}px) translate(-50%, -50%)">
+    
 </div>
